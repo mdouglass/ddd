20de178fb2594efbd4448cde44764cc8025643c1
Matthew Douglass <matthew@globalworldwide.com>
2024-12-10 22:05:22 -0800

    Support optionally returning the plain text of the ics
    This makes debugging a bit easier in the browser.

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,17 @@ export interface Env {
   DDD_ICS: string
 }
 
-export default {
-  async fetch(request, env, ctx): Promise<Response> {
-    return new Response(await convert(env.DDD_ICS), {
-      headers: { 'content-type': 'text/calendar' },
-    })
-  },
-} satisfies ExportedHandler<Env>
+const fetch: ExportedHandlerFetchHandler<Env, unknown> = async (request, env, ctx) => {
+  console.log(request.url)
+  console.dir(URL.parse(request.url))
+  return new Response(await convert(env.DDD_ICS), {
+    headers: {
+      'content-type':
+        URL.parse(request.url)?.searchParams.get('mime') === 'plain'
+          ? 'text/plain'
+          : 'text/calendar',
+    },
+  })
+}
+
+export default { fetch } satisfies ExportedHandler<Env>
